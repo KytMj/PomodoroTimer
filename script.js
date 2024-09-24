0fad31cab254572cd315a1a77317044dd4c561a8
Maelys <joret.maelys@gmail.com>
2024-09-24 12:51:28 +0200

    Change color when switch between work and break

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let timer = document.getElementById("timer");
 let workingTime = true;
 
 let customizedBreakTime = 5 ;
-let customizedWorkingTime = 1 ;
+let customizedWorkingTime = 0.1 ;
 
 let time = customizedWorkingTime * 60 - 1;
 timer.innerText = `${customizedWorkingTime}:00`
@@ -27,10 +27,11 @@ function startAndStop(){
         buttonRestart = false;
         document.getElementById("classButton").className = "fa-solid fa-play";
     }
+    changeColor();
 }
 
 function decreaseTime(){
-
+    changeColor();
     let minutes = parseInt( time / 60, 10 );
     let seconds = parseInt( time % 60, 10 );
 
@@ -45,4 +46,46 @@ function decreaseTime(){
     else{
         time = time - 1;
     }
+}
+
+function changeColor(){
+    if(workingTime == true){
+        document.getElementsByClassName("travail")[0].style.backgroundColor = 'orange';
+        document.getElementsByClassName("pause")[0].style.backgroundColor = 'red';
+    }
+    else{
+        document.getElementsByClassName("pause")[0].style.backgroundColor = 'blue';
+        document.getElementsByClassName("travail")[0].style.backgroundColor = 'red';
+    }
+}
+
+// Get the modal
+var modal = document.getElementById("customizeTimeModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("parametre");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+function customizeTime(){
+    let resultat = document.getElementById("parameters");
+    console.log(resultat);
 }
