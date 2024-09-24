513f67be4261981cfc83c62a95e1c7a7d2f23863
Maelys <joret.maelys@gmail.com>
2024-09-24 10:29:27 +0200

    Add style to the page + change stop button

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let timer = document.getElementById("timer");
-let time = 25 * 60;
-
-
+let time = 25 * 60 - 1;
+let buttonActivation = false;
+let interval;
 
 function decreaseTime(){
     let minutes = parseInt( time / 60, 10 );
@@ -11,8 +11,23 @@ function decreaseTime(){
     seconds = seconds < 10 ? "0" + seconds : seconds;
 
     timer.innerText = `${minutes}:${seconds}`;
-    time = time <= 0 ? 0 : time - 1
+    time = time <= 0 ? clearInterval(interval) : time - 1
 }
 
 let start = document.getElementById("start")
-start.addEventListener("click", () => {setInterval(decreaseTime,1000)});
+start.addEventListener("click", startAndStop);
+
+function startAndStop(){
+    if(buttonActivation == false){
+        interval = setInterval(decreaseTime, 1000);
+        buttonActivation = true;
+        document.getElementById("classButton").className = "fa-solid fa-rotate-left";
+    }
+    else{
+        clearInterval(interval);
+        time = 25 * 60 - 1;
+        timer.innerText = "25:00";
+        buttonActivation = false;
+        document.getElementById("classButton").className = "fa-solid fa-play";
+    }
+}
